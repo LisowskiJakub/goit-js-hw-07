82c37d0c34a46f2264bc3a4546a7305e26eb86fb
Jakub Lisowski <lisowski.jakub@gmail.com>
2022-07-25 22:21:36 +0200

    finished second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+    const markup = `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.original}" data-source="${item.original}" alt="${item.description}"/>
+    </a> `
+   
+
+    gallery.insertAdjacentHTML('beforeend', markup);
+
+})
+gallery.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+		return;
+	}   
+    let lightbox = new SimpleLightbox('.gallery a',);
+
+      
+  });
+
